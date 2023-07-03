eb40c20015a841fec9fa6ad3a4b82dc1b4f4f510
Guebre Aziz <aziz@MacBook-Pro-di-Guebre.local>
2023-07-03 23:52:58 +0200

    db connection estabilised

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,13 +1,22 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from "express";
+require("dotenv").config();
 
 const app: Application = express();
+const morgan = require("morgan");
+const connectDB = require("./services/db/connection");
 
 const PORT: number = 9000;
 
-app.use('/', (req: Request, res: Response): void => {
-  res.status(200).json({ title: 'Hello API!' });
+// HTTP request logger middleware
+app.use(morgan("common"));
+
+// mongoDB connection
+connectDB();
+
+app.use("/", (req: Request, res: Response): void => {
+  res.status(200).json({ title: "Hello API!" });
 });
 
 app.listen(PORT, (): void => {
-  console.log('SERVER IS UP ON PORT:', PORT);
+  console.log("SERVER IS UP ON PORT:", PORT);
 });
